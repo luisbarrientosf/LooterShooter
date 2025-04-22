e1e06320298adb09a8ab6f80c40d34c465853c8c
Luis Barrientos <luis.barrientos.faj@gmail.com>
2025-04-22 01:18:51 -0400

    feat(coins): slowly decrease spin

--- Assets/Scripts/Items/CoinMagnet.cs
+++ Assets/Scripts/Items/CoinMagnet.cs
@@ -5,6 +5,7 @@ public class CoinMagnet : MonoBehaviour {
   public float magnetRange = 3f;
   public float magnetSpeed = 3f;
   public float slowdownRate = 3.5f;
+  public float spinSpeed = 3f;
   public GameObject magnetTrailEffect;
 
   private Transform player;
@@ -13,16 +14,26 @@ public class CoinMagnet : MonoBehaviour {
   private float currentSpeed = 0f;
   private Vector3 lastDirection = Vector3.zero;
 
+  private float maxScaleX;
+  private float currentSpinMultiplier = 1f;
+  private float normalSpinMultiplier = 1f;
+  private float magnetizedSpinMultiplier = 2.2f;
+  public float spinTransitionSpeed = 2f;
+  private float spinTimer = 0f;
+
   void Start() {
     PlayerController playerController = FindFirstObjectByType<PlayerController>();
     if (playerController != null) {
       player = playerController.transform;
     }
+    maxScaleX = transform.localScale.x;
+
   }
 
   void Update() {
     if (player == null) return;
 
+    Spin();
     float distance = Vector2.Distance(transform.position, player.position);
 
     if (distance < magnetRange) {
@@ -38,6 +49,7 @@ public class CoinMagnet : MonoBehaviour {
       currentSpeed = Mathf.Max(0, currentSpeed - slowdownRate * Time.deltaTime);
       if (currentSpeed < 0.1f && activeTrail != null) {
         activeTrail.GetComponent<ParticleSystem>().Stop(true);
+        isMagnetized = false;
       }
     }
 
@@ -53,6 +65,16 @@ public class CoinMagnet : MonoBehaviour {
       player.GetComponent<PlayerInventory>().AddCoin();
       Destroy(gameObject);
     }
+  }
+
+  void Spin() {
+    float targetMultiplier = isMagnetized ? magnetizedSpinMultiplier : normalSpinMultiplier;
+    currentSpinMultiplier = Mathf.MoveTowards(currentSpinMultiplier, targetMultiplier, spinTransitionSpeed * Time.deltaTime);
+    spinTimer += Time.deltaTime * spinSpeed * currentSpinMultiplier;
+
+    float t = Mathf.PingPong(spinTimer, 1f);
+    float scaleX = Mathf.Lerp(0f, maxScaleX, t);
+    transform.localScale = new Vector3(scaleX, transform.localScale.y, transform.localScale.z);
   }
 
   void AddParticlesWhenMoving() {
