b898c6b49d827004264204f93dbf50d34b55a988
Luis Barrientos <luis.barrientos.faj@gmail.com>
2025-04-22 19:34:38 -0400

    refactor: pawn appareance

--- Assets/Scripts/Pawn/PawnAppareance.cs
+++ Assets/Scripts/Pawn/PawnAppareance.cs
@@ -10,43 +10,37 @@ public class PawnAppearance : MonoBehaviour {
   public GameObject[] rightEyesVariants;
   public GameObject[] hatsVariants;
 
-  public void ChangeEyes(int leftEyeIndex, int rightEyeIndex) {
-    if (leftEyeRenderer != null && leftEyesVariants.Length > 0 && leftEyeIndex < leftEyesVariants.Length) {
-      leftEyeRenderer.sprite = leftEyesVariants[leftEyeIndex].GetComponent<SpriteRenderer>().sprite;
-    }
-
-    if (rightEyeRenderer != null && rightEyesVariants.Length > 0 && rightEyeIndex < rightEyesVariants.Length) {
-      rightEyeRenderer.sprite = rightEyesVariants[rightEyeIndex].GetComponent<SpriteRenderer>().sprite;
-    }
-  }
-
-  public void ChangeHat(int hatIndex) {
-    if (hatRenderer != null && hatsVariants.Length > 0 && hatIndex < hatsVariants.Length) {
-      hatRenderer.sprite = hatsVariants[hatIndex].GetComponent<SpriteRenderer>().sprite;
-      hatRenderer.transform.localPosition = hatsVariants[hatIndex].GetComponent<AppearanceItem>().correctLocalPosition;
-      hatRenderer.transform.localScale = hatsVariants[hatIndex].GetComponent<AppearanceItem>().correctLocalScale;
-    }
-  }
-
   void Start() {
-
     leftEyesVariants = Resources.LoadAll<GameObject>("Sprites/Player/Eyes/LeftEyes");
     rightEyesVariants = Resources.LoadAll<GameObject>("Sprites/Player/Eyes/RightEyes");
     hatsVariants = Resources.LoadAll<GameObject>("Sprites/Player/Hats");
 
-    int randomEyeIndex = Random.Range(0, leftEyesVariants.Length);
-    ChangeEyes(randomEyeIndex, randomEyeIndex);
-    int randomHatIndex = Random.Range(0, hatsVariants.Length);
-    ChangeHat(randomHatIndex);
+    RandomizeAppearance();
   }
 
   void Update() {
     if (Input.GetKeyDown(KeyCode.R)) {
-      int randomPosition = Random.Range(0, leftEyesVariants.Length);
-      ChangeEyes(randomPosition, randomPosition);
-      int randomHatIndex = Random.Range(0, hatsVariants.Length);
-      ChangeHat(randomHatIndex);
-      Start();
+      RandomizeAppearance();
+    }
+  }
+
+  void RandomizeAppearance() {
+    int randomEyesIndex = Random.Range(0, leftEyesVariants.Length);
+    AttachAppearanceItem(leftEyeRenderer, leftEyesVariants[randomEyesIndex]);
+    AttachAppearanceItem(rightEyeRenderer, rightEyesVariants[randomEyesIndex]);
+
+    int randomHatIndex = Random.Range(0, hatsVariants.Length);
+    AttachAppearanceItem(hatRenderer, hatsVariants[randomHatIndex]);
+  }
+
+  public void AttachAppearanceItem(SpriteRenderer targetRenderer, GameObject itemPrefab) {
+    targetRenderer.sprite = itemPrefab.GetComponent<SpriteRenderer>().sprite;
+    AppearanceItem appareanceOptions = itemPrefab.GetComponent<AppearanceItem>();
+
+    if (appareanceOptions != null) {
+      targetRenderer.transform.localPosition = appareanceOptions.correctLocalPosition;
+      targetRenderer.transform.localScale = appareanceOptions.correctLocalScale;
+      targetRenderer.transform.localEulerAngles = appareanceOptions.correctLocalRotation;
     }
   }
 }
