b4a66fc081d49f76db6de47e9e44371ab76b85fe
Luis Barrientos Fajardo <luis.barrientos.faj@gmail.com>
2025-05-12 14:53:02 -0400

    feat(scenes): added pause and you died menu

--- Assets/Scripts/TestSceneLoader.cs
+++ Assets/Scripts/TestSceneLoader.cs
@@ -5,5 +5,7 @@ public class TestSceneLoader : MonoBehaviour {
   void Start() {
     SceneManager.LoadScene("HUD", LoadSceneMode.Additive);
     SceneManager.LoadScene("Inventory", LoadSceneMode.Additive);
+    SceneManager.LoadScene("Pause Menu", LoadSceneMode.Additive);
+    SceneManager.LoadScene("You Died", LoadSceneMode.Additive);
   }
 }
