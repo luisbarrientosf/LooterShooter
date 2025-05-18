79150defec4216a940be0a3429ed0b18bc1e5424
Luis Barrientos Fajardo <luis.barrientos.faj@gmail.com>
2025-05-17 20:28:02 -0400

    fix(scenes): scene loading

--- Assets/Scripts/Scene Loaders/GameplaySceneLoader.cs
+++ Assets/Scripts/Scene Loaders/GameplaySceneLoader.cs
@@ -1,15 +1,51 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameplaySceneLoader : MonoBehaviour {
   void Awake() {
-    Debug.Log("Gameplay> Loading Map Generator");
     GameManager.Instance.mapGenerator = PerlinMapGenerator.Instance;
-    Debug.Log("Gameplay> Loading Player Health");
     GameManager.Instance.player = PlayerHealth.Instance;
+    StartCoroutine(LoadGameScenes());
   }
 
   void Start() {
-    Debug.Log("Gameplay> START GAME");
     GameManager.Instance.StartGame();
   }
+
+  IEnumerator LoadGameScenes() {
+    if (!GameManager.Instance.isTestGame) {
+      yield return LoadSceneSafe(Scenes.HUD);
+      yield return LoadSceneSafe(Scenes.Inventory);
+      yield return LoadSceneSafe(Scenes.PauseMenu);
+      yield return LoadSceneSafe(Scenes.YouDied);
+    }
+  }
+
+  IEnumerator LoadSceneSafe(string sceneName) {
+    Debug.Log("Attempting to load scene: " + sceneName);
+
+    if (!Application.CanStreamedLevelBeLoaded(sceneName)) {
+      Debug.LogError($"SceneLoader > Scene '{sceneName}' not found in Build Settings!");
+      yield break;
+    }
+
+    if (SceneManager.GetSceneByName(sceneName).isLoaded) {
+      Debug.LogWarning($"SceneLoader > Scene '{sceneName}' is already loaded.");
+      yield break;
+    }
+
+    AsyncOperation loadOp = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+    if (loadOp == null) {
+      Debug.LogError($"SceneLoader > LoadSceneAsync returned null for '{sceneName}'");
+      yield break;
+    }
+
+    while (!loadOp.isDone) {
+      yield return null;
+    }
+
+    Debug.Log("SceneLoader > Successfully loaded scene: " + sceneName);
+  }
+
 }
--- Assets/Scripts/Scene Loaders/SceneLoader.cs
+++ Assets/Scripts/Scene Loaders/SceneLoader.cs
@@ -5,38 +5,13 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour {
   public static string targetScene = "Gameplay";
 
-  void Start() {
+  void Awake() {
     StartCoroutine(LoadGameScenes());
   }
 
   IEnumerator LoadGameScenes() {
-    Debug.Log("Scene Loader> Loading Main Scene");
     AsyncOperation mainLoad = SceneManager.LoadSceneAsync(targetScene, LoadSceneMode.Additive);
-    while (!mainLoad.isDone)
-      yield return null;
-
-    if (!GameManager.Instance.isTestGame) {
-
-      Debug.Log("Scene Loader> Loading Inventory");
-      AsyncOperation inventoryLoad = SceneManager.LoadSceneAsync(Scenes.Inventory, LoadSceneMode.Additive);
-      while (!inventoryLoad.isDone)
-        yield return null;
-
-      Debug.Log("Scene Loader> Loading Pause Menu");
-      AsyncOperation pauseMenuLoad = SceneManager.LoadSceneAsync(Scenes.PauseMenu, LoadSceneMode.Additive);
-      while (!pauseMenuLoad.isDone)
-        yield return null;
-
-      Debug.Log("Scene Loader> Loading You Died Menu");
-      AsyncOperation youDiedMenuLoad = SceneManager.LoadSceneAsync(Scenes.YouDied, LoadSceneMode.Additive);
-      while (!youDiedMenuLoad.isDone)
-        yield return null;
-
-      Debug.Log("Scene Loader> Loading HUD");
-      AsyncOperation hudLoad = SceneManager.LoadSceneAsync(Scenes.HUD, LoadSceneMode.Additive);
-      while (!hudLoad.isDone)
-        yield return null;
-    }
+    while (!mainLoad.isDone) yield return null;
 
     SceneManager.UnloadSceneAsync(Scenes.Loading);
   }
